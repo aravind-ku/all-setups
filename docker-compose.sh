2b2e3c742d1fa86382801f3ce32095673620873d
Aravind <135971349+aravind-ku@users.noreply.github.com>
2026-05-10 18:29:24 +0530

    Update docker-compose.sh to install CLI plugin
    Add installation steps for Docker Compose CLI plugin

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,3 +3,15 @@ ls /usr/local/bin/
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
+
+
+
+------------------------------LATEST----------------------------------------------------
+mkdir -p ~/.docker/cli-plugins/
+
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+  -o ~/.docker/cli-plugins/docker-compose
+
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+docker compose version
